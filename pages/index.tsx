c44fd8cea0e993cd64faa64b2b90679f1b6f1258
mindsgn <seni.tembe@protonmail.com>
2022-04-08 16:45:24 +0200

    update website

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { Phone, Main, Footer, Info } from '../components';
 import { Box } from '@chakra-ui/react';
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <Box
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Box
       maxW='100vw'
       overflow={'hidden'}  
         cursor='pointer'>
         <Main />
         <Footer />
-    </Box>
+      </Box>
+    </>
   );
 };
 
