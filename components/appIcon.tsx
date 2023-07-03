3e500ab9691654280da1ff378e72000888375331
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-07-03 13:42:12 +0200

    update (#7)
    * update
    
    * update

--- components/appIcon.tsx
+++ components/appIcon.tsx
@@ -1,28 +1,30 @@
-import { Box, Button} from '@chakra-ui/react';
-import GooglePlay from '../public/assets/images/google-play-light.svg'
-import Image from 'next/image';
+import { Box, Button } from "@chakra-ui/react";
+import GooglePlay from "../public/assets/images/google-play-light.svg";
+import Image from "next/image";
 
 export const appIcons = (): JSX.Element => {
-    
-    const goTo = (link:string) => {
-        console.log(link);
-        window.open(link, '_blank');
-    }
+  const goTo = (link: string) => {
+    console.log(link);
+    window.open(link, "_blank");
+  };
 
-    return(
-        <Box
-            padding={5}
-            width='100vw'
-            display='flex'
-            flexWrap='wrap'
-            justifyContent={'space-evenly'}
-            background='white'
-            cursor={'pointer'}>
-                <Box
-                    onClick={() => goTo("https://play.google.com/store/apps/details?id=seni.tembe.orbyt")}>
-                    <Image
-                        src={GooglePlay}/>
-                </Box>
-        </Box>
-    )
-}
+  return (
+    <Box
+      padding={5}
+      width="100vw"
+      display="flex"
+      flexWrap="wrap"
+      justifyContent={"space-evenly"}
+      background="white"
+      cursor={"pointer"}
+    >
+      <Box
+        onClick={() =>
+          goTo("https://play.google.com/store/apps/details?id=seni.tembe.orbyt")
+        }
+      >
+        <Image alt="google play button" src={GooglePlay} />
+      </Box>
+    </Box>
+  );
+};
